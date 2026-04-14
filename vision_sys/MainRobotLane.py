36088869ae9cdb6996664bc714e8b8ebdc18ebb0
switftyJim-png <jamesnguyen.maine@gmail.com>
2026-04-13 22:11:31 -0400

    remove motor; add serial connect

--- vision_sys/MainRobotLane.py
+++ vision_sys/MainRobotLane.py
@@ -1,11 +1,12 @@
 from MotorModule import Motor
 from LaneDetectionModule import getLaneCurve
 import WebcamModule
- 
+import serial
+
 ##################################################
-motor = Motor(2,3,4,17,22,27) ##likely need to replace this with serial connection: Motor('/dev/ttyUSB0', 9600)
+#motor = Motor(2,3,4,17,22,27) ##likely need to replace this with serial connection: Motor('/dev/ttyUSB0', 9600)
 ##################################################
- 
+arduino = serial.Serial(port='COM3', baudrate=115200, timeout=1); # Establish connection to Arduino
 def main():
  
     img = WebcamModule.getImg()
@@ -18,13 +19,16 @@ def main():
     #print(curveVal)
     if curveVal>0:
         sen =1.7
+        cmd = "R"
         if curveVal<0.05: curveVal=0
     else:
+        cmd = "L"
         if curveVal>-0.08: curveVal=0
-    motor.move(0.20,-curveVal*sen,0.05)
+    arduino.write(f"{cmd}{abs(curveVal):.2f}\n".encode('utf-8'))  # Send command to Arduino
+    #motor.move(0.20,-curveVal*sen,0.05)
     #cv2.waitKey(1)
      
- 
+
 if __name__ == '__main__':
     while True:
         main()
