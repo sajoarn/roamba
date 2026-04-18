ac23ae7ddb2a31a7c08324942a73ef92c9ae58a3
switftyJim-png <jamesnguyen.maine@gmail.com>
2026-04-18 14:26:50 -0400

    get camera to work

--- vision_sys/WebcamModule.py
+++ vision_sys/WebcamModule.py
@@ -1,23 +1,27 @@
 import cv2
-
-#cap =cv2.VideoCapture(0)#pulls video feed from camera
-#need to connect to wifi and then use stream url to get video feed from robot's camera
-#probably need to do that in wifi
+import numpy as np
 
 class Webcam:
     def __init__(self, stream_url='http://192.168.4.1:81/stream'):
         self.cap = cv2.VideoCapture(stream_url)
+        
         if not self.cap.isOpened():
-            print("\nOpenCV cannot open the camera URL.")
+            print("\n[ERROR] OpenCV cannot open the camera URL.")
             print("Are you sure the PC is connected to the ELEGOO Wi-Fi?\n")
         else:
             print("Connection made to camera.\n")
         
-    def getImg(self,display=False, size=[480,240]):
-        _,img = self.cap.read()
-        img = cv2.resize(img, (size[0],size[1]))
+    def getImg(self, display=False, size=[480,240]):
+        ret, img = self.cap.read()
+  
+        if not ret or img is None:
+            print("WARNING: Camera feed dropped a frame! Waiting for voltage to stabilize...")
+            # Return a blank black image so cv2.resize doesn't explode
+            return np.zeros((size[1], size[0], 3), dtype=np.uint8)
+
+        img = cv2.resize(img, (size[0], size[1]))
 
         if display:
-            cv2.imshow("IMG",img)
+            cv2.imshow("IMG", img)
+            cv2.waitKey(1)
         return img
-
